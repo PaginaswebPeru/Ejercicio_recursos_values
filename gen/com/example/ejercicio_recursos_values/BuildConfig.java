d6775e961827f930dc54bb025e3fe683be61e027
Jorge Sandoval <jorge.sandoval@paginasweb.pe>
2013-12-16 14:43:11 -0500

    primer commit

---
+++ gen/com/example/ejercicio_recursos_values/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.ejercicio_recursos_values;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
